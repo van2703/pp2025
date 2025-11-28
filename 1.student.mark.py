6330791bbc9c187e598f116ea3ddd04a614f43b3
vanco <vanphamx41@gmail.com>
2025-11-28 16:38:02 +0700

    lab1

---
+++ 1.student.mark.py
@@ -0,0 +1,100 @@
+students = []
+courses = []
+marks = {}
+def number_of_students():
+    n = int(input("Number of students: "))
+    return n
+def students(n):
+    for i in range(n):
+        print(f"\nstudent {i+1}")
+        id = input("student ID: ")
+        name = input("name: ")
+        dob = input("date of Birth: ")
+
+        student = {
+            "id": id,
+            "name": name,
+            "doB": dob
+        }
+        students.append(student)
+
+def number_of_courses():
+    m = int(input("Number of courses: "))
+    return m
+
+def courses(m):
+    for i in range(m):
+        print(f"\ncourse {i+1}")
+        id = input("course ID: ")
+        name = input("course name: ")
+        course = {
+            "id": id,
+            "name": name
+        }
+        courses.append(course)
+
+def marks():
+    for c in courses:
+        print(f"{c['id']} - {c['name']}")
+    course_id = input("Enter course ID: ")
+    if course_id not in marks:
+        marks[course_id] = {}
+    print(f"\nInput marks for course {course_id}")
+    for s in students:
+        mark = float(input(f"Mark for {s['name']} (ID: {s['id']}): \n"))
+        marks[course_id][s["id"]] = mark
+
+def list_students():
+    print("student list\n")
+    for s in students:
+        print(f"ID: {s['id']}, Name: {s['name']}, DoB: {s['dob']}")
+
+def list_courses():
+    print("\ncourse list")
+    for c in courses:
+        print(f"ID: {c['id']}, Name: {c['name']}")
+
+def student_marks():
+    for c in courses:
+        print(f"{c['id']} - {c['name']}")
+
+    course_id = input("Enter course ID: ")
+
+    if course_id not in marks:
+        print("No marks for this course yet.")
+        return
+
+    print(f"\nmark for course {course_id}")
+    for s in students:
+        id = s["id"]
+        if id in marks[course_id]:
+            print(f"{s['name']} (ID: {id}) : {marks[course_id][id]}")
+        else:
+            print(f"{s['name']} (ID: {id}) : No mark")
+def main():
+    n = number_of_students()
+    students(n)
+    m = number_of_courses()
+    courses(m)
+    while True:
+        print("1. List students")
+        print("2. List courses")
+        print("3. Input marks for a course")
+        print("4. Show student marks for a course")
+        print("0. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            list_students()
+        elif choice == "2":
+            list_courses()
+        elif choice == "3":
+            marks()
+        elif choice == "4":
+            student_marks()
+        elif choice == "0":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option. Try again.")
